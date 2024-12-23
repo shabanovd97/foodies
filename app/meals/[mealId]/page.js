0caa6928415bd497276087f1ad7a0d9d36ef4a6f
Dmytro Shabanov <shabanovd97@gmail.com>
2024-12-23 20:35:58 +0100

    add meal details

--- app/meals/[mealId]/page.js
+++ app/meals/[mealId]/page.js
@@ -1,9 +1,36 @@
-export default function MealDetails({ params }) {
+import Image from 'next/image';
+
+import { getMeal } from '@lib/meals';
+
+import styles from './page.module.css';
+
+export default async function MealDetails({ params: { mealId } }) {
+  const meal = await getMeal(mealId);
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        {`Meal Details Page for ${params.mealId}`}
-      </h1>
-    </main>
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
